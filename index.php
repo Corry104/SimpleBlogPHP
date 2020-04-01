1887c50d2afb0db71923e5b6c89e5dca032d3a97
Corrado <44187946+Corry104@users.noreply.github.com>
2020-03-31 19:03:59 -0700

    index shows blog posts

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
 
+    require 'classes/Database.php';
 
+    $database = new Database;
+
+    $database->query('SELECT * FROM posts');
+
+    $rows = $database->resultset();
 ?>
+
+<h1>Posts</h1>
+<div>
+    <?php foreach($rows as $row) : ?>
+        <div>
+            <!-- Title of the post -->
+            <h3><?php echo$row['title']; ?></h3>
+
+            <!-- Body of the post -->
+            <p><?php echo$row['body']; ?></p>
+        </div>
+    <?php endforeach; ?>
+</div>
+
+
