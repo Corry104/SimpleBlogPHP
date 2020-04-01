8430e8449faee19c23619230be9564f9937b6673
Corrado <44187946+Corry104@users.noreply.github.com>
2020-03-31 19:36:48 -0700

    insert into database without reloading

--- index.php
+++ index.php
@@ -4,10 +4,42 @@
 
     $database = new Database;
 
-    $database->query('SELECT * FROM posts');
+    $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
+    if($post['submit']){
+        $title = $post['title'];
+        $body = $post['body'];
+
+        // Insert Posts into database
+        $database->query('INSERT INTO posts (title, body) VALUES(:title, :body)');
+        $database->bind(':title', $title);
+        $database->bind(':body', $body);
+        $database->execute();
+
+        if($database->lastInsertId()){
+            echo '<p>Post Added!</p>';
+        }
+    }
+
+    // Make posts be able to appear without refreshing the browser
+
+    $database->query('SELECT * FROM posts');
     $rows = $database->resultset();
 ?>
+
+<h1>Add Post</h1>
+<form method='post' action='<?php $_SERVER['PHP_SELF']; ?>'>
+    <label>Post Title</label>
+    <br/>
+    <input type="text" name="title" placeholder="Add a title..."  />
+    <br>
+    <br>
+    <label>Post Body</label>
+    <br>
+    <textarea name="body"></textarea>
+    <br>
+    <input type="submit" name='submit' value='Submit' />
+</form>
 
 <h1>Posts</h1>
 <div>
